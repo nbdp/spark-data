ae66cac4dee5d4f9b9d9cc555f339306cfd6aa9b
shijingui <shijingui@jd.com>
2018-09-05 11:05:51 +0800

    format

--- src/main/scala/data/GroupByTest.scala
+++ src/main/scala/data/GroupByTest.scala
@@ -1,22 +1,3 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-// scalastyle:off println
-package org.spark.data
 
 import java.util.Random
 
@@ -29,10 +10,10 @@ object GroupByTest {
   def main(args: Array[String]) {
     val spark = SparkSession
       .builder
-      .appName("GroupBy Test")
+      .appName("GroupBy Test").master("local[2]")
       .getOrCreate()
 
-    val numMappers = if (args.length > 0) args(0).toInt else 2
+    val numMappers = 100
     val numKVPairs = if (args.length > 1) args(1).toInt else 1000
     val valSize = if (args.length > 2) args(2).toInt else 1000
     val numReducers = if (args.length > 3) args(3).toInt else numMappers
@@ -48,12 +29,11 @@ object GroupByTest {
       arr1
     }.cache()
     // Enforce that everything has been calculated and in cache
-    pairs1.count()
+    println(pairs1.count())
 
     println(pairs1.groupByKey(numReducers).count())
-
+    print(pairs1.toDebugString)
     spark.stop()
   }
 }
 
-// scalastyle:on println
