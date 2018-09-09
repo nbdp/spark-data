36ce3fc8eb982e6916a87b7b9c39540a45f3cdbc
krisjin <krisibm@163.com>
2018-09-09 20:42:56 +0800

    streaming

---
+++ src/main/scala/data/streaming/one.scala
@@ -0,0 +1,18 @@
+package data.streaming
+
+import org.apache.spark.SparkConf
+import org.apache.spark.streaming.{Seconds, StreamingContext}
+
+/**
+  * @author shijingui on 2018/9/9
+  */
+object one {
+
+
+  def main(args: Array[String]): Unit = {
+
+    val conf = new SparkConf().setAppName("one streaming").setMaster("local[2]")
+    val ssc = new StreamingContext(conf, Seconds(5));
+    ssc.start();
+  }
+}
